9290ab97063f3041fe3e800d657a2967f111a3c2
Andrei Soare <andrei.soare@gmail.com>
2011-04-21 10:32:54 +0300

    Improve test

--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 #/usr/bin/env bash
 
 ant compile
+sleep 2
 java -cp .:build:lib:lib/log4j-1.2.16.jar src.Sharix andrei &
+sleep 2
 java -cp .:build:lib:lib/log4j-1.2.16.jar src.Sharix vlad &
+sleep 2
 java -cp .:build:lib:lib/log4j-1.2.16.jar src.Sharix ion &
+sleep 2
